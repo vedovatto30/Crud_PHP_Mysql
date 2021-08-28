4787897769383fdfcf76683824c5163a69885abe
Fernando Vitor <63013289+vedovatto30@users.noreply.github.com>
2021-08-27 23:30:10 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,60 +1,65 @@
-<?php
+<?php 
 require_once 'CLASSES/usuarios.php';
-$user = new Usuario();
-define('host_name', 'localhost');
-define('host_user', 'root');
-define('host_pwd', "").
-define('db_name', 'projeto_login');
+$u = new Usuario();
 ?>
+
 <html lang="pt-br">
 <head>
-  <meta charset="utf-8"/> 
-  <title>Projeto Login</title>
-  <link rel="stylesheet"  href="css/stilo.css">
+	<meta charset="utf-8"/>
+	<title>Projeto Login</title>
+	<link rel="stylesheet" href="css/stilo.css">
+	<meta name="viewport" content="width=device-width">
 </head>
 <body>
-	<div id="corpo-form">
+<div id="corpo-form">
 	<h1>Entrar</h1>
-    <form method="POST">
-    	<input type="email" placeholder="Usuario" name="email">
-        <input type="password" placeholder="Senha" name="senha">
-        <input type="submit" value="ACESSAR">
-        <a href="cadastrar.php">Ainda não é inscrito? <strong>Cadastre-se</strong></a>
-    </form>
+	<form method="POST">
+		<input type="email" placeholder="Usuário" name="email">
+		<input type="password" placeholder="Senha" name="senha">
+		<input type="submit" value="ACESSAR">
+		<a href="cadastrar.php">Ainda não esta inscrito?<strong>Cadastre-se!</strong></a>
+	</form>
 </div>
 <?php
- if(isset($_POST['email']))
-    {
-    $email = addslashes($_POST['email']);
-    $senha = addslashes($_POST['senha']);
-
-    if (!empty($email) && !empty($senha))
-     {
-        $u -> conectar(db_name, host_name,host_user,host_pwd);
-        if ($u -> msgErro == ""){
-            if($u -> logar($email, $senha)){
-                header("location: areaPrivada.php");
-            } else {
-                ?>
-                <div class="msg-erro"> Email ou Senha estão incorretos </div>
-            <?php
-            }
-        } else {
-            ?>
-            <div class="msg-erro">
-                <?php
-                echo "ERRO: " . $u -> msgErro;
-                ?>
-            </div>
-        <?php
-        }
-
-    }else {
-        ?>
-        <div class="msg-erro"> Por gentileza. Preencha os dois campos </div>
-    <?php
-        }
-    }
+if(isset($_POST['email']))
+{
+	$email = addslashes($_POST['email']);
+	$senha = addslashes($_POST['senha']);
+	if(!empty($email) && !empty($senha))
+	{
+		$u->conectar("projeto_login","localhost","root","");
+		if($u->msgErro == "")
+		{
+			if($u->logar($email,$senha))
+			{
+				header("location:areaPrivada.php");
+			}
+			else
+			{
+				?>
+				<div class="msg-erro">
+					Email e/ou senha estão incorretos!
+				</div>
+				<?php
+			}
+		}
+		else
+		{
+			?>
+			<div class="msg-erro">
+				<?php echo "Erro: ".$u->msgErro; ?>
+			</div>
+			<?php
+		}
+	}else
+	{
+		?>
+		<div class="msg-erro">
+			Preencha todos os campos!
+		</div>
+		<?php
+	}
+}
 ?>
 </body>
 </html>
